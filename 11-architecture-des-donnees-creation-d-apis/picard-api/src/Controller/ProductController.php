cfe8018c7df739e3c876ad63cf0088102bfd055d
cynthiaapura <cynthia.apura@hotmail.fr>
2025-08-04 18:57:41 +0200

    feat: add rate

--- 11-architecture-des-donnees-creation-d-apis/picard-api/src/Controller/ProductController.php
+++ 11-architecture-des-donnees-creation-d-apis/picard-api/src/Controller/ProductController.php
@@ -5,7 +5,10 @@ namespace App\Controller;
 use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
 
 final class ProductController extends AbstractController
 {
@@ -18,7 +21,7 @@ final class ProductController extends AbstractController
             'products' => $products,
         ]);
     }
-    
+
     #[Route('/products/{id}', name: 'app_product_detail')]
     public function detail(int $id, ProductRepository $productRepository): Response
     {
@@ -32,4 +35,27 @@ final class ProductController extends AbstractController
             'product' => $product,
         ]);
     }
+    
+    #[Route('/products/{id}/rate', name: 'app_product_rate', methods: ['POST'])]
+    public function rate(int $id, Request $request, ProductRepository $productRepository, EntityManagerInterface $em): RedirectResponse
+    {
+        $product = $productRepository->find($id);
+        if (!$product) {
+            throw $this->createNotFoundException('Produit non trouvé');
+        }
+
+        $rating = (float) $request->request->get('rating');
+        if ($rating < 0 || $rating > 5) {
+            $this->addFlash('error', 'Note invalide. Elle doit être entre 0 et 5.');
+            return $this->redirectToRoute('app_product_detail', ['id' => $id]);
+        }
+
+        $product->setRating($rating);
+        $em->flush();
+
+        $this->addFlash('success', 'Merci pour votre note');
+
+        return $this->redirectToRoute('app_product_detail', ['id' => $id]);
+    }
+
 }
