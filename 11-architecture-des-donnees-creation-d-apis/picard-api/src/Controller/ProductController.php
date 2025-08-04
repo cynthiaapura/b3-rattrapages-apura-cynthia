134b373cc67b34f4d25a92d66181365953bf6979
cynthiaapura <cynthia.apura@hotmail.fr>
2025-08-04 16:59:28 +0200

    feat(front): list and details of products

--- 11-architecture-des-donnees-creation-d-apis/picard-api/src/Controller/ProductController.php
+++ 11-architecture-des-donnees-creation-d-apis/picard-api/src/Controller/ProductController.php
@@ -2,17 +2,34 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
 final class ProductController extends AbstractController
 {
-    #[Route('/product', name: 'app_product')]
-    public function index(): Response
+    #[Route('/products', name: 'app_product')]
+    public function index(ProductRepository $productRepository): Response
     {
+        $products = $productRepository->findAll();
+
         return $this->render('product/index.html.twig', [
-            'controller_name' => 'ProductController',
+            'products' => $products,
+        ]);
+    }
+    
+    #[Route('/products/{id}', name: 'app_product_detail')]
+    public function detail(int $id, ProductRepository $productRepository): Response
+    {
+        $product = $productRepository->find($id);
+
+        if (!$product) {
+            throw $this->createNotFoundException('Produit non trouvé');
+        }
+
+        return $this->render('product/detail.html.twig', [
+            'product' => $product,
         ]);
     }
 }
